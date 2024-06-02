fd94c44694e9bf5979cba436d83a5bf50db91178
Your Name <you@example.com>
2024-06-02 16:45:01 +0000

    TicketPriceCalculator blue print created

---
+++ TicketPriceCalculator/TicketPriceCalculator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TicketPriceCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            double ticketPrice;
+
+            Console.Write("Enter your age please: ");
+            if (int.TryParse(Console.ReadLine(), out age))
+            {
+                if (age <= 12 || age >= 65)
+                {
+                    ticketPrice = 7.0;
+                    Console.WriteLine($"The price of your ticket is GHC{ticketPrice}.");
+                }
+                else
+                {
+                    ticketPrice = 10.0;
+                    Console.WriteLine($"THe price of your ticket is GHC{ticketPrice}.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid age");
+            }
+        }
+    }
+}
