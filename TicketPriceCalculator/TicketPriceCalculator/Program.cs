c9f30ed39075a1bd72cd0815c5c9d8913af49391
Your Name <you@example.com>
2024-06-02 16:51:21 +0000

    TicketPriceCalculator functionality added

--- TicketPriceCalculator/TicketPriceCalculator/Program.cs
+++ TicketPriceCalculator/TicketPriceCalculator/Program.cs
@@ -6,26 +6,38 @@ namespace TicketPriceCalculator
     {
         static void Main(string[] args)
         {
-            int age;
-            double ticketPrice;
-
-            Console.Write("Enter your age please: ");
-            if (int.TryParse(Console.ReadLine(), out age))
+            while (true)
             {
-                if (age <= 12 || age >= 65)
+                Console.Write("Enter your age please: ");
+                string input = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(input))
                 {
-                    ticketPrice = 7.0;
-                    Console.WriteLine($"The price of your ticket is GHC{ticketPrice}.");
+                    return;
+                }
+
+                int age;
+
+
+
+                if (int.TryParse(Console.ReadLine(), out age))
+                {
+                    double ticketPrice;
+                    if (age <= 12 || age >= 65)
+                    {
+                        ticketPrice = 7.0;
+                        Console.WriteLine($"The price of your ticket is GHC{ticketPrice}.");
+                    }
+                    else
+                    {
+                        ticketPrice = 10.0;
+                        Console.WriteLine($"THe price of your ticket is GHC{ticketPrice}.");
+                    }
                 }
                 else
                 {
-                    ticketPrice = 10.0;
-                    Console.WriteLine($"THe price of your ticket is GHC{ticketPrice}.");
+                    Console.WriteLine("Please enter a valid age");
                 }
-            }
-            else
-            {
-                Console.WriteLine("Please enter a valid age");
             }
         }
     }
